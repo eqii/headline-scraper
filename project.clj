6669f0fccef03f8a840e6fa1865fa0dddae31c28
eqii <eero.j.palomaki@aalto.fi>
2016-08-19 20:26:03 +0300

    refactoring + cached http requests

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [hiccup "1.0.5"]
                  [enlive "1.1.5"]
-                 [org.clojure/data.json "0.2.6"]]            
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/core.memoize "0.5.8"]]
   :main ^:skip-aot headline-scraper.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/headline_scraper/core.clj
+++ src/headline_scraper/core.clj
@@ -1,18 +1,32 @@
-(ns tm-news.core
+(ns headline-scraper.core
   (:gen-class)
   (:use [hiccup.core])
   (:require [net.cgrand.enlive-html :as html]
             [clojure.data.json :as json]
-            [clojure.string :as string]))
+            [clojure.core.memoize :as memoize]))
 
 
 (def iltalehti-url "http://www.iltalehti.fi")
 (def iltasanomat-url "http://www.iltasanomat.fi")
 
+(def iltalehti-selectors { :titles [:div#iltab_luetuimmat-kaikki1 :> :p :> :a :span.list-title]
+                           :links [:div#iltab_luetuimmat-kaikki1 :> :p :> :a] })
+
+(def iltasanomat-selectors { :titles [:div.most-read :> :div.tabs-content :> :ol.is-most-read-articles-list
+                                      :li.list-item :> :div.content :> :p]
+                             :links [:div.most-read :> :div.tabs-content :>
+                                     :ol.is-most-read-articles-list :> :li.list-item :a] })
+
 (defn fetch-url [url encoding]
   (-> url java.net.URL.
       .getContent (java.io.InputStreamReader. encoding)
       html/html-resource))
+
+(def fetch-iltasanomat
+  (memoize/ttl #(fetch-url iltasanomat-url "UTF-8") {} :ttl/threshold 1000))
+
+(def fetch-iltalehti
+  (memoize/ttl #(fetch-url iltalehti-url "ISO-8859-1") {} :ttl/threshold 1000))
 
 (defn parse-href [a]
   (:href (:attrs a)))
@@ -20,26 +34,26 @@
 (defn parse-content [node]
   (first (:content node)))
 
-(defn get-is-links []
-  (take 10 
+(defn get-iltasanomat-links []
+  (take 10
     (map #(str iltasanomat-url (parse-href %))
-      (html/select (fetch-url iltasanomat-url "UTF-8") [:div.most-read :> :div.tabs-content :> :ol.is-most-read-articles-list :> :li.list-item :a]))))
+      (html/select (fetch-iltasanomat) (:links iltasanomat-selectors)))))
 
-(defn get-is-titles []
-  (take 10 
+(defn get-iltasanomat-titles []
+  (take 10
     (map parse-content
-      (html/select (fetch-url iltasanomat-url "UTF-8") [:div.most-read :> :div.tabs-content :> :ol.is-most-read-articles-list :li.list-item :> :div.content :> :p]))))
+      (html/select (fetch-iltasanomat) (:titles iltasanomat-selectors )))))
 
-(defn get-il-links []
-  (map parse-href 
-    (html/select (fetch-url iltalehti-url "ISO-8859-1") [:div#iltab_luetuimmat-kaikki1 :> :p :> :a])))
+(defn get-iltalehti-links []
+  (map parse-href
+    (html/select (fetch-iltalehti) (:links iltalehti-selectors))))
 
-(defn get-il-titles []
+(defn get-iltalehti-titles []
   (map parse-content
-    (html/select (fetch-url iltalehti-url "ISO-8859-1") [:div#iltab_luetuimmat-kaikki1 :> :p :> :a :span.list-title])))
+    (html/select (fetch-iltalehti) (:titles iltalehti-selectors))))
 
 (defn get-most-read []
-  (let [actions [get-il-titles get-il-links get-is-titles get-is-links]
+  (let [actions [get-iltalehti-titles get-iltalehti-links get-iltasanomat-titles get-iltasanomat-links]
         futures (doall (map #(future (apply % [])) actions))
         results (map deref futures)
         [il-titles il-links is-titles is-links] results
